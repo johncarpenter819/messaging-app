352eadfa72bdf494c06690911faefcd0b36c38b2
John Carpenter <122576866+johncarpenter819@users.noreply.github.com>
2025-10-02 18:10:22 -0400

    pg library to tell render use connectionstring and still be able to connect locally

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,13 +1,23 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: String(process.env.DB_PASSWORD),
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  ssl: { rejectUnauthorized: false },
-});
+let connectionConfig;
+
+if (process.env.DATABASE_URL) {
+  connectionConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  connectionConfig = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: String(process.env.DB_PASSWORD),
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+  };
+}
+
+const pool = new Pool(connectionConfig);
 
 export default pool;
